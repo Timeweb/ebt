517b4c1712bfe3a3e6e764991f6fb14c8626aabc
sotskiy <sotskiy@gmail.com>
2014-10-13 13:35:55 +0400

    Добавил немецкую локализацию

--- src/ebt_date.erl
+++ src/ebt_date.erl
@@ -1,6 +1,17 @@
 -module(ebt_date).
 
+-export([date_str/0]).
 -export([date_str_ru/0]).
+-export([date_str_de/0]).
+
+date_str() ->
+  {ok, #{ notifications := Locale }} = application:get_env(portal, locale),
+  case Locale of
+    "ru" ->
+      date_str_ru();
+    "de" ->
+      date_str_de()
+  end.
 
 date_str_ru() ->
     Months = #{
@@ -20,3 +31,22 @@ date_str_ru() ->
     YearStr = " года",
     {{Y, M, D}, _} = erlang:localtime(),
     unicode:characters_to_binary(integer_to_list(D) ++ maps:get(M, Months) ++ integer_to_list(Y) ++ YearStr).
+
+date_str_de() ->
+  Months = #{
+        1 => " Januar ",
+        2 => " Februar ",
+        3 => " März ",
+        4 => " April ",
+        5 => " Mai ",
+        6 => " Juni ",
+        7 => " Juli ",
+        8 => " August ",
+        9 => " September ",
+        10 => " Oktober ",
+        11 => " November ",
+        12 => " Dezember "
+      },
+  YearStr = "",
+  {{Y, M, D}, _} = erlang:localtime(),
+  unicode:characters_to_binary(integer_to_list(D) ++ maps:get(M, Months) ++ integer_to_list(Y) ++ YearStr).
